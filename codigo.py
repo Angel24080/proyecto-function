708fbaf497270e7c146c87982f188fd1eec3fab3
Luis <angel.checag@gmail.com>
2025-04-20 16:20:35 -0500

    Subiendo el archivo de python

---
+++ codigo.py
@@ -0,0 +1,13 @@
+def crear_mensajes(nombre, apellido, edad, anio_nacimiento):
+    # Crear el primer mensaje con el nombre y apellido
+    mensaje1 = f"Hola {nombre} {apellido}. ¿Cómo estás?"
+    
+    # Crear el segundo mensaje con los datos de la edad y año de nacimiento
+    mensaje2 = f"Ya veo que tienes {edad} años porque naciste en {anio_nacimiento}."
+    
+    # Imprimir ambos mensajes
+    print(mensaje1)
+    print(mensaje2)
+
+# Invocar la función con tus datos
+crear_mensajes("Norman", "Wild", 16, 2001)
